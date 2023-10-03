c2758bd0646a4ca0a936a9f0c47668708d6593e5
tchild1 <childtanner@gmail.com>
2023-10-02 20:31:09 -0600

    initial commit

---
+++ src/starter/chess/Board.java
@@ -0,0 +1,18 @@
+package chess;
+
+public class CBoard  implements  ChessBoard {
+    @Override
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+
+    }
+
+    @Override
+    public ChessPiece getPiece(ChessPosition position) {
+        return null;
+    }
+
+    @Override
+    public void resetBoard() {
+
+    }
+}
